bbbcb684923c3f01aa00dad3b9f49ad008a50868
dmi3coder <dmi3coder@gmail.com>
2020-10-10 17:53:07 +0300

    Initial commit

---
+++ src/main/java/io/github/openbahn/schedule/OpenbahnScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.openbahn.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbahnScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbahnScheduleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/openbahn/schedule/OpenbahnScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.openbahn.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbahnScheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
